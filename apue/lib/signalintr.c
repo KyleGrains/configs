2bf40e7bbf3fcdc69d4d8ee704b0abaae563108c
Kyle <kylelsun@gmail.com>
2021-05-06 23:02:42 +0800

    Add sigal() implemented by sigaction

--- apue/lib/signalintr.c
+++ apue/lib/signalintr.c
@@ -15,3 +15,18 @@ signal_intr(int signo, Sigfunc *func)
 		return(SIG_ERR);
 	return(oact.sa_handler);
 }
+Sigfunc *
+signal_intr_test(int signo, Sigfunc *func)
+{
+	struct sigaction act, oact;
+
+	act.sa_handler = func;
+	sigemptyset(&act.sa_mask);
+	act.sa_flags = 0;
+#ifdef SA_INTERRUPT
+	act.sa_flags |= SA_INTERRUPT;
+#endif
+	if(sigaction(signo, &act, &oact) < 0)
+		return(SIG_ERR);
+	return(oact.sa_handler);
+}
