66a9767bd2413864d4812bd93eaa5b467adab1c9
Kyle <kyle.grains@gmail.com>
2022-04-10 17:39:57 +0800

    Add neovim config

---
+++ config/setup.sh
@@ -0,0 +1,10 @@
+#install nvim
+wget https://github.com/neovim/neovim/releases/download/nightly/nvim-linux64.deb
+sudo apt install nvim-linux64.deb
+
+#install PlugInstall
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+
+#install ccls
+sudo snap install ccls --classic
